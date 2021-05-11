5d9438a64e7f002c1a286ce64df70eb31e7a3246
freddy <freddy@gmail.com>
2021-05-11 19:55:19 +0800

    11

---
+++ server/config/database.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+	//'url': 'mongodb://202.43.239.11:27017',
+	'url': 'mongodb://127.0.0.1:27017',
+	'options': {
+		//'user':   '00',
+		//'pass':   '000',
+		'dbName': 'admin',
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
